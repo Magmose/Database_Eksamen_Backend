843e404eebf7fe67c4156ea4a0016bd24bdbaea4
CONVERGENS\mak <mak@convergens.lan>
2021-05-24 13:10:56 +0200

    Overall movie, Add list/single user top followed

--- src/main/java/mmr/redis/Redis.java
+++ src/main/java/mmr/redis/Redis.java
@@ -1,9 +1,12 @@
 package mmr.redis;
 
+import mmr.dto.User;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Tuple;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
 import java.util.Set;
 
 public class Redis {
@@ -13,42 +16,95 @@ public class Redis {
     // Fiks build data
 
     private Jedis jedis;
+    private LocalDate currentdate;
+    private final static int DEFAULT_OUTPUT_TOP = 3;
 
     public Redis(String host, int port) {
+        currentdate = LocalDate.now();
         jedis = new Jedis(host, port);
     }
 
     public boolean incDay(String movie) {
-        LocalDate currentdate = LocalDate.now();
-        jedis.zincrby(currentdate.toString(), 1, movie);
+        var response = jedis.sismember("top:total:movie", currentdate.toString());
+        if (response) {
+            jedis.zincrby(currentdate.toString(), 1, movie);
+        } else {
+            try (var tran = jedis.multi()) {
+                tran.sadd("top:total:movie", currentdate.toString());
+                tran.zincrby(currentdate.toString(), 1, movie);
+                tran.exec();
+            }
+        }
         return true;
     }
 
-    public Set<Tuple> getTopWeek() {
+
+    public Set<Tuple> getTopToday(int outputNumber) {
+        return jedis.zrangeWithScores(currentdate.toString(), -outputNumber, -1);
+    }
+
+    public Set<Tuple> getTopToday() {
+        return getTopToday(DEFAULT_OUTPUT_TOP);
+    }
+
+    public Set<Tuple> getTopWeek(int outputNumber) {
         String[] dates = new String[7];
         for (int i = 0; i <= 6; i++) {
-            String date = LocalDate.now().minusDays(i).toString();
+            String date = currentdate.minusDays(i).toString();
             dates[i] = date;
         }
         String dstKey = dates[0] + ">" + dates[6];
-
-
         jedis.zunionstore(dstKey, dates);
-        Set<Tuple> response = jedis.zrangeWithScores(dstKey, -3, -1);
-        return response;
+        return jedis.zrangeWithScores(dstKey, -outputNumber, -1);
     }
 
-    public Set<Tuple> getTopToday(){
-        LocalDate currentdate = LocalDate.now();
-        return jedis.zrangeWithScores(currentdate.toString(), -3, -1);
+    public Set<Tuple> getTopWeek() {
+        return getTopWeek(DEFAULT_OUTPUT_TOP);
     }
 
-    public long addTopFollowed(String id, String username,int followed){
-       return jedis.zadd("top:users",followed,username+":"+id);
+    public Set<Tuple> getTopMonth(int outputNumber) {
+        YearMonth yearMonth = YearMonth.of(currentdate.getYear(), currentdate.getMonth());
+        int daysInMonth = yearMonth.lengthOfMonth();
+        String[] dates = new String[daysInMonth];
+        for (int i = 0; i < daysInMonth; i++) {
+            LocalDate date = LocalDate.of(currentdate.getYear(), currentdate.getMonthValue(), i + 1);
+            dates[i] = date.toString();
+        }
+        String dstKey = dates[0] + ">" + dates[daysInMonth - 1];
+        jedis.zunionstore(dstKey, dates);
+        return jedis.zrangeWithScores(dstKey, -outputNumber, -1);
     }
 
-    public Set<Tuple> getTopFollowed(){
-    return jedis.zrangeWithScores("top:users", -3, -1);
+    public Set<Tuple> getTopMonth() {
+        return getTopMonth(DEFAULT_OUTPUT_TOP);
+    }
+
+    public Set<Tuple> getTopOverall(int outputNumber) {
+        var response = jedis.smembers("top:total:movie").toArray(new String[0]);
+        String dstKey = "top:total:movie:score";
+        jedis.zunionstore(dstKey, response);
+        return jedis.zrangeWithScores(dstKey, -outputNumber, -1);
+    }
+
+    public Set<Tuple> getTopOverall() {
+        return getTopOverall(DEFAULT_OUTPUT_TOP);
+    }
+
+    public void addTopFollowed(User user) {
+        jedis.zadd("top:users", user.getFollowed(), user.getUsername() + ":" + user.getId());
+    }
+
+    public void addListTopFollowed(List<User> users) {
+        try (var tran = jedis.multi()) {
+            for (User user : users) {
+                tran.zadd("top:users", user.getFollowed(), user.getUsername() + ":" + user.getId());
+            }
+            tran.exec();
+        }
+    }
+
+    public Set<Tuple> getTopFollowed() {
+        return jedis.zrangeWithScores("top:users", -3, -1);
     }
 
 
@@ -56,18 +112,14 @@ public class Redis {
     public boolean randomData(String movie) {
 
         for (int i = 0; i <= 6; i++) {
-            String date = LocalDate.now().minusDays(i).toString();
-            String member = "test_"+i;
-
+            String date = currentdate.minusDays(i).toString();
+            String member = "test_" + i;
             var re = jedis.zincrby(date, 1, member);
-           // System.out.println("Works: " + re+"     key."+date);
-
-
         }
         return true;
     }
 
-    public void fulshDB(){
-      jedis.flushAll();
+    public void fulshDB() {
+        jedis.flushAll();
     }
 }
--- src/main/java/mmr/redis/SetupRedis.java
+++ src/main/java/mmr/redis/SetupRedis.java
@@ -1,7 +1,10 @@
 package mmr.redis;
 
+import mmr.dto.User;
 import redis.clients.jedis.Tuple;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class SetupRedis {
@@ -11,29 +14,61 @@ public class SetupRedis {
 
         redis.randomData("test");
 
-        System.out.println("________ Week");
 
         redis.incDay("YEEW the movie");
         redis.incDay("YEEW the movie");
         redis.incDay("YEEW the movie");
         redis.incDay("Kage patrolien");
         redis.incDay("Kage patrolien");
-        Set<Tuple> response = redis.getTopWeek();
-        response.forEach(k -> System.out.println(k));
+
+       ;
+
+        System.out.println("________ addTopUser");
+
+        redis.addTopFollowed(new User("123ID", "MangusTester", 5));
+        redis.addTopFollowed(new User("1231233ID", "Kage", 4));
+        redis.addTopFollowed(new User("1223453ID", "Ost", 3));
+        redis.addTopFollowed(new User("1235345ID", "Syltetøj", 2));
+        redis.addTopFollowed(new User("123ID", "MangusTester", 195436435));
+
+        User user1 = new User("12312","kaj",312);
+         User user2 = new User("12346557312","kasdgaj",412312);
+         User user3 = new User("12356712","xekadsaj",13142312);
+         User user4 = new User("123546712","ewqkaj",313122);
+         User user5 = new User("12375612","asdkaj",63612);
+
+         List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        redis.addListTopFollowed(users);
+
+        Set<Tuple> response2 = redis.getTopFollowed();
+        response2.forEach(y -> System.out.println(y));
 
         System.out.println("________ Today");
 
         Set<Tuple> response1 = redis.getTopToday();
         response1.forEach(x -> System.out.println(x));
 
-        System.out.println("________ addTopUser");
+        System.out.println("________ Week");
 
-        redis.addTopFollowed("123ID", "MangusTester", 5);
-        redis.addTopFollowed("1231233ID", "Kage", 4);
-        redis.addTopFollowed("1223453ID", "Ost", 3);
-        redis.addTopFollowed("1235345ID", "Syltetøj", 2);
-        redis.addTopFollowed("123ID", "MangusTester", 199);
-        Set<Tuple> response2 = redis.getTopFollowed();
-        response2.forEach(y -> System.out.println(y));
+        Set<Tuple> response = redis.getTopWeek();
+        response.forEach(k -> System.out.println(k));
+
+
+        System.out.println("________ Month");
+
+        Set<Tuple> response3 = redis.getTopMonth();
+        response3.forEach(x -> System.out.println(x));
+
+        System.out.println("________ overAll");
+
+        Set<Tuple> response4 = redis.getTopOverall();
+        response4.forEach(x -> System.out.println(x));
+
+
     }
 }
